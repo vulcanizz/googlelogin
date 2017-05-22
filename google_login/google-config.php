3ef08cf9b17528d346bb3d0d694a2dfb3431f231
vulcanizz <saesoe_k@silpakorn.edu>
2017-05-23 01:53:35 +0700

    Add files via upload

--- google_login/google-config.php
+++ google_login/google-config.php
@@ -3,9 +3,9 @@ include_once 'src/google/Google_Client.php';
 include_once 'src/google/contrib/Google_Oauth2Service.php';
 
 
-$clientId = '351533939784-5j6bqm6br7ro0p6fpsfjjfb820vsufk9.apps.googleusercontent.com'; //Google client ID
-$clientSecret = 'T4ILDFLKTGL2ywZxz2rHt6pY'; //Google client secret
-$gRedirectURL = 'www.mytest.me/google_login/'; //Callback URL
+$clientId = '321788750722-so20900fc4bj9vr9m37fru8lu30di198.apps.googleusercontent.com'; //Google client ID
+$clientSecret = 'LCr4VazGs-B5116B6GzYYumt'; //Google client secret
+$gRedirectURL = 'http://www.mytest.me/google_login'; //Callback URL
 
 $gClient = new Google_Client();
 $gClient->setApplicationName('xxxx');
--- google_login/index.php
+++ google_login/index.php
@@ -8,29 +8,28 @@ include_once('google-login.php');
 ?>
 
 <!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/styleggl.css">
 <head>
-	<title>13570340</title>
+	<title>13570125</title>
 </head>
 <body> 
 	<?php
 		if(isset($_SESSION['token'])){
 		?>
-		<div class="box-info2">
-			<h1>ข้อมูลรายละเอียด</h1>
+		<div class="box-wrapper">
+			<h1>Account Details</h1>
 			<div class="box-img">
-			<img src="<?php echo $google_img ; ?>" alt="">
+				<img src="<?php echo $google_img ; ?>" alt="">
 			</div>
-			<p>ชื่อ google :  <?php echo $google_name ; ?></p>
-			<p>ชื่อจริง :  <?php echo $google_fname ; ?></p>
-			<p>ชื่อสกุล :  <?php echo $google_lname ; ?></p>
-			<p>เพศ :  <?php echo $google_gender ; ?></p>
-			<p>email :  <?php echo $google_email ; ?></p>
-
-			
+			<div style="float: left; margin-left: 200px; margin-top: 10px;">
+				<p>ชื่อ google :  <?php echo $google_name ; ?></p>
+				<p>ชื่อจริง :  <?php echo $google_fname ; ?></p>
+				<p>ชื่อสกุล :  <?php echo $google_lname ; ?></p>
+				<p>เพศ :  <?php echo $google_gender ; ?></p>
+				<p>email :  <?php echo $google_email ; ?></p>
+			</div>
         		<?php
-        			echo '<a href="logout.php" class="myButton"> <button class="box-but"> Logout with google </button>  </a>';
+        			echo '<a href="logout.php" class="myButton"> <button class="box-out"> Logout from google </button>  </a>';
         		?>
     		
 
@@ -38,6 +37,7 @@ include_once('google-login.php');
 		}
 
 	?>
+		</div>
 	
 <body>
 </body>
